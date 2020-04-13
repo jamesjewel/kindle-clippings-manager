4cfbe3fe758d20688bbe188d128ded8e63ff4f84
Jewel James <flakyhermit@protonmail.ch>
2020-04-13 19:53:17 +0530

    file close and clear clutter

--- main.py
+++ main.py
@@ -7,22 +7,20 @@
 ## Change these variables to configure the program
 FILENAME = "My Clippings.txt"
 CLIPPING_END_STRING = "=========="
+
 # Global Variables
 fileDir = "./" # End with a /
+lines = []
 
 # Open the file, read the data as a string
 file = open(fileDir + FILENAME, "r")
-dataStr = file.read()
-# print(dataStr)
+lines = file.readlines()
+file.close()
 
 # Parsing text
 curLine = 0
 startLine = 0
 stopLine = 0
-
-count = 0
-file.seek(0)
-lines = file.readlines()
 
 for index, line in enumerate(lines):
 #   print("{}: {}".format(index, line))
@@ -30,5 +28,4 @@ for index, line in enumerate(lines):
         stopLine = index
         for i in range(startLine, stopLine):
             print(lines[i], end='')
-        print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
         startLine = stopLine + 1
