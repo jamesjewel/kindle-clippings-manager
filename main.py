e9ac92b33fd9c1866a7f8edd83c9ee783889d44c
Jewel James <flakyhermit@protonmail.ch>
2020-04-14 14:14:27 +0530

    implemented regexp search

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 # Imports
-# import os
+import re
+import json
 
 ## CONFIGURATION
 ## Change these variables to configure the program
@@ -22,10 +23,37 @@ curLine = 0
 startLine = 0
 stopLine = 0
 
+count = 0
+
+def parseBlock(startLine, stopLine):
+   # line 1: BOOK_NAME (AUTHOR_NAME)
+   line2 = lines[startLine].strip()
+   pattern = re.compile(r"([\s\S]+)\(([\s\S]+)\)")
+   result = pattern.match(line2)
+   book = {}
+   if result != None:
+       book['name'] = result.group(1).strip()
+       book['author'] = result.group(2).strip()
+#      print(result.group(1), result.group(2), sep='\t')
+
+   line2 = lines[startLine+1].strip()
+   book['posInfo'] = line2
+   pattern = re.compile(r"- Your (Highlight|Note|Bookmark) (on|at) (location|page) (\d+)(-?)(\d*) \|? (location (\d+)(-?)(\d*)){0,1}")
+   result = pattern.match(line2)
+   if result != None:
+       print(result.groups())
+   # line 3: highlight
+   line2 = lines[startLine + 3].strip()
+   book['highlight'] = line2
+
+#  print(book)
+
 for index, line in enumerate(lines):
 #   print("{}: {}".format(index, line))
     if line.rstrip() == CLIPPING_END_STRING:
+        count = count + 1
+        print(count)
         stopLine = index
-        for i in range(startLine, stopLine):
-            print(lines[i], end='')
+        parseBlock(startLine, stopLine)
         startLine = stopLine + 1
+
