914c6ea2aec88a01a817fc4e52d18aa36e28a05a
Jewel James <flakyhermit@protonmail.ch>
2020-04-15 12:38:36 +0530

    added date parsing

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ stopLine = 0
 count = 0
 
 class Clip:
-class Clip:
     """The clipping class definitio n"""
     def __init__(self, _title, _author, _text, _time):
         self.title = _title
@@ -44,29 +43,34 @@ class Clip:
 def parseBlock(startLine, stopLine):
    # line 1: Book info
    curLine = lines[startLine].strip()
-   pattern = re.compile(r"([\s\S]+)\(([\s\S]+)\)")
-   result = pattern.match(curLine)
-   clipObj = Clip()
-   if result != None:
-       clipObj.title = result.group(1).strip()
-       clipObj.author = result.group(2).strip()
+   p = re.compile(r"([\s\S]+)\(([\s\S]+)\)")
+   res = p.match(curLine)
+   clipObj = Clip("","","","")
+   if res != None:
+       clipObj.title = res.group(1).strip()
+       clipObj.author = res.group(2).strip()
    # line 2: Metadata
    curLine = lines[startLine+1].strip()
-   pattern = re.compile(r"- Your (Highlight|Note|Bookmark) (on|at) (location|page) (\d+)(-?)(\d*)( \| location (\d+)(-?)(\d*))? \| Added on ([a-zA-Z]{3,6}day), (\d{1,2}) ([a-zA-Z]+) (\d{4}) (\d\d):(\d\d):(\d\d)")
-   result = pattern.match(curLine)
-#  if result != None:
-       #print(result.groups())
+   p = re.compile(r"- Your (Highlight|Note|Bookmark) (on|at) (location|page) (\d+)(-?)(\d*)( \| location (\d+)(-?)(\d*))? \| Added on ([a-zA-Z]{3})[a-zA-Z]{,3}day, (\d{1,2}) ([a-zA-Z]{3})[a-zA-Z]+ (\d{4}) (\d\d):(\d\d):(\d\d)")
+   res = p.match(curLine)
+   # extracting timestamp
+   # strptime('Fri Mar 01 23:38:40 2019')
+   timestring = '{weekday} {month} {day}'
+   timestring = timestring.format(weekday=res.group(11), month=res.group(13), day=res.group(12))
+   print(timestring)
+#  if res != None:
+#     print(res.groups())
    # line 3: Highlight
    curLine = lines[startLine + 3].strip()
    clipObj.text = curLine
 #  print(clipObj)
-   clipObj.printClip()
+#  clipObj.printClip()
 # The main program begins here:
 for index, line in enumerate(lines):
 #   print("{}: {}".format(index, line))
     if line.rstrip() == CLIPPING_END_STRING:
         count = count + 1
-        print(count)
+#       print(count)
         stopLine = index
         parseBlock(startLine, stopLine)
         startLine = stopLine + 1
